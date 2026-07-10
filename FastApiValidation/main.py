dbdcd79be9737c5b2bca9794eb5dc4e88d6265db
manyia34 <mahajanmanyia790@gmail.com>
2026-07-10 11:17:15 +0530

    Response model

---
+++ FastApiValidation/main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+app = FastAPI()
+
+books = [
+    {
+        "id": 1,
+        "title": "The Guide",
+        "author": "R K Narayan",
+        "genre": "Fiction",
+        "language": "English",
+        "internal_note": "Added by admin",
+        "created_by": "trainer"
+    }
+]
+
+# ====================================
+# Pydanctic model for creating books 
+# ====================================
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    language: str
+    genre: str
+
+
+# ===================================================================
+# Response Model is used when we to show some specific information
+# ===================================================================
+class BookResopnse(BaseModel):
+    id : int
+    title: str
+    author: str
+    genre: str
+    language: str
+
+# ======================================
+# Message Response Model
+# ======================================
+class BookActionResponse(BaseModel):
+    message: str
+    book: BookResopnse
+
+# ===================================================================
+# list[BookResopnse] is used because we return all the books record
+# ==================================================================
+@app.get('/books', response_model=list[BookResopnse])
+def get_books():
+    return books
+
+# ============================================================
+# Response model is always written in the decorator
+# ============================================================
+@app.get('/books/{book_id}', response_model=BookResopnse)
+def get_books_by_id(book_id: int):
+    for i in books:
+        if i["id"] == book_id:
+            return i
+    raise HTTPException(status_code=404, detail="Book Not Found!")
+
+
+# ======================================================
+# creation of the new books using the response mddel
+# ======================================================
+@app.post('/books', response_model=BookActionResponse,status_code=201)
+def add_new_book(book: BookCreate):
+    new_id = max((book["id"] for book in books), default=0)+1
+    new_book = {
+        "id": new_id,
+        "title": book.title,
+        "genre": book.genre,
+        "language": book.language,
+        "author": book.author,
+        "internal_note": "Added by admin",
+        "created_by": "trainer"
+    }
+    books.append(new_book)
+    return {
+        "message": "Book Added Successfully!",
+        "book": new_book
+    }
