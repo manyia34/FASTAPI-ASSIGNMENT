139517b9720706811fc39587f6c7e2a1db76f67e
manyia34 <mahajanmanyia790@gmail.com>
2026-07-10 11:26:09 +0530

    Status header file , Response Model

--- FastApiValidation/main.py
+++ FastApiValidation/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException , status  
 from pydantic import BaseModel
 app = FastAPI()
 
@@ -27,7 +27,7 @@ class BookCreate(BaseModel):
 # ===================================================================
 # Response Model is used when we to show some specific information
 # ===================================================================
-class BookResopnse(BaseModel):
+class BookResponse(BaseModel):
     id : int
     title: str
     author: str
@@ -39,30 +39,31 @@ class BookResopnse(BaseModel):
 # ======================================
 class BookActionResponse(BaseModel):
     message: str
-    book: BookResopnse
+    book: BookResponse
 
 # ===================================================================
-# list[BookResopnse] is used because we return all the books record
+# list[BookResponse] is used because we return all the books record
 # ==================================================================
-@app.get('/books', response_model=list[BookResopnse])
+@app.get('/books', response_model=list[BookResponse])
 def get_books():
     return books
 
 # ============================================================
 # Response model is always written in the decorator
 # ============================================================
-@app.get('/books/{book_id}', response_model=BookResopnse)
+@app.get('/books/{book_id}', response_model=BookResponse)
 def get_books_by_id(book_id: int):
     for i in books:
         if i["id"] == book_id:
             return i
-    raise HTTPException(status_code=404, detail="Book Not Found!")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book Not Found!")
 
+# HTTP_404_NOT_FOUND is used to show the error message when the book is not found in the name constraints
 
 # ======================================================
 # creation of the new books using the response mddel
 # ======================================================
-@app.post('/books', response_model=BookActionResponse,status_code=201)
+@app.post('/books', response_model=BookActionResponse,status_code=status.HTTP_201_CREATED)
 def add_new_book(book: BookCreate):
     new_id = max((book["id"] for book in books), default=0)+1
     new_book = {
